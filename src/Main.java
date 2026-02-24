489db71dca30731c779a483f6c0a67acfbf972fd
jeannette uwimbabazi <janetmbabazi885@gmail.com>
2026-02-24 09:05:41 +0200

    feat: add abstract calculateTuition to Student

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student s1 = new Student("Jeannette", "janet@gmail.com", "ST001", 3.6, "Software Engineering");
-        Instructor i1 = new Instructor("Mr. Paul", "paul@gmail.com", "Software Engineering");
-
-        System.out.println(s1.getName() + " - " + s1.getRole() + " - GPA: " + s1.getGpa());
-        System.out.println(i1.getName() + " - " + i1.getRole() + " - Dept: " + i1.getDepartment());
-    }
+       }
 }
