246c3fb5662b20e08c8be537d0e5fd008d8f25f2
jeannette uwimbabazi <janetmbabazi885@gmail.com>
2026-02-24 12:25:10 +0200

    lab3: add data folder and FileManager

---
+++ src/persistence/FileManager.java
@@ -0,0 +1,7 @@
+package persistence;
+
+public class FileManager {
+    public static final String STUDENTS_FILE = "data/students.csv";
+    public static final String COURSES_FILE = "data/courses.csv";
+    public static final String ENROLLMENTS_FILE = "data/enrollments.csv";
+}
